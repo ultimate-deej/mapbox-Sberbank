fa23a255ea62370c34da303445402e458ef8f540
darius <manaev@me.com>
2015-02-18 01:10:22 +0300

    setter should default to unanimated

--- MapView/Map/RMMapView.m
+++ MapView/Map/RMMapView.m
@@ -964,7 +964,7 @@
 
 - (void)setCenterCoordinate:(CLLocationCoordinate2D)centerCoordinate
 {
-    [self setCenterProjectedPoint:[_projection coordinateToProjectedPoint:centerCoordinate]];
+    [self setCenterCoordinate:centerCoordinate animated:NO];
 }
 
 - (void)setCenterCoordinate:(CLLocationCoordinate2D)centerCoordinate animated:(BOOL)animated
@@ -990,7 +990,7 @@
 
 - (void)setCenterProjectedPoint:(RMProjectedPoint)centerProjectedPoint
 {
-    [self setCenterProjectedPoint:centerProjectedPoint animated:YES];
+   [self setCenterProjectedPoint:centerProjectedPoint animated:NO];
 }
 
 - (void)setCenterProjectedPoint:(RMProjectedPoint)centerProjectedPoint animated:(BOOL)animated
